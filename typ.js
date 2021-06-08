ff2641f35e6ced3190d2c13f2f6b76d9c7aba685
SimBed <dansimbed@gmail.com>
2021-06-08 14:32:50 +0100

    keep score

--- typ.js
+++ typ.js
@@ -1,35 +1,22 @@
-var ImageDatabase = {
+var imageDatabase = {
   'tiger': 'https://picsum.photos/id/1074/300',
   'yoda': 'https://picsum.photos/id/1025/300',
   'puppy': 'https://picsum.photos/id/237/300',
 }
+var score = 0;
+var rounds = 0;
+var success = 0;
 
 document.onload = initialise();
 
 function initialise() {
-  document.getElementById('tiger').src = addQueryParam(ImageDatabase['tiger'], "grayscale");
-  document.getElementById('yoda').src = addQueryParam(ImageDatabase['yoda'], "grayscale");
-  document.getElementById('puppy').src = addQueryParam(ImageDatabase['puppy'], "grayscale");
-  // document.getElementById('human').src = addQueryParam(ImageDatabase['yoda'], "grayscale");
-  // document.getElementById('comp').src = addQueryParam(ImageDatabase['yoda'], "grayscale");
-  // document.getElementById('test').src = addQueryParam(ImageDatabase['yoda'], "grayscale");
+  document.getElementById('tiger').src = addQueryParam(imageDatabase['tiger'], "grayscale");
+  document.getElementById('yoda').src = addQueryParam(imageDatabase['yoda'], "grayscale");
+  document.getElementById('puppy').src = addQueryParam(imageDatabase['puppy'], "grayscale");
   gsap.timeline()
     .to("#choose img", {duration: 0.5, autoAlpha: 1, stagger: 0.2})
     .to(".header", {duration: 1, autoAlpha: 1});
   }
-
-function play_again() {
-  gsap.timeline()
-    .to("#result img", {duration: 0.5, autoAlpha: 0, stagger: 0.2})
-  // document.getElementById('result').style.visibility = "hidden";
-    // document.getElementById('choose').style.display = "block";
-    .set("#choose", {display: "block"})
-  .to("#choose img", {duration: 0.5, autoAlpha: 1})
-    .to(".header", {duration: 1, autoAlpha: 1});
-        document.getElementById('result').style.visibility = "hidden";
-  // document.getElementById('choose').style.display = "block";
-  // document.getElementsByClassName('header')[0].style.visibility = "visible";
-}
 
 function rsp(yourChoice) {
   var humanChoice, botChoice;
@@ -37,31 +24,49 @@ function rsp(yourChoice) {
   botChoice = imagePick();
   console.log(humanChoice + ' ' + botChoice);
   outcome = decideWinner(humanChoice, botChoice);
+  score += get_score(outcome['humanResult']);
+  rounds += 1;
+  success = parseFloat(100 * score / rounds).toFixed(0)+"%";
   gsap.timeline()
     .to("#choose img", {duration: 0.5, autoAlpha: 0, stagger: 0.2})
     .set("#choose", {display: "none"}, "+=0.1")
     .set("#result", {visibility: "visible"})
     .set(".header", {visibility: "hidden"})
-
-  // document.getElementById('choose').style.display = "none";
-  // document.getElementById('result').style.visibility = "visible";
-  // document.getElementsByClassName('header')[0].style.visibility = "hidden";
-
   display(humanChoice, botChoice, outcome)
   gsap.to("#message", {duration:1.5, text: outcome['message'], delay:0.1});
-}
-
-function addQueryParam(a, b) {
-  return a + "/?" + b
+  gsap.to("#score", {duration:1.5, text: score + " from " + rounds + ", " + success + " success" , delay:0.1});
 }
 
 function display(humanChoice, botChoice, outcome) {
-  document.getElementById('human').src = addQueryParam(ImageDatabase[humanChoice], distort(outcome['humanResult']));
-  document.getElementById('comp').src = addQueryParam(ImageDatabase[botChoice], distort(compResult(outcome['humanResult'])));
+  document.getElementById('human').src = addQueryParam(imageDatabase[humanChoice], distort(outcome['humanResult']));
+  document.getElementById('comp').src = addQueryParam(imageDatabase[botChoice], distort(compResult(outcome['humanResult'])));
+  document.getElementById('result').style.display = "block";
   gsap.timeline()
     .to("#human", {duration: 0.5, autoAlpha: 1})
     .to("#comp", {duration: 0.5, autoAlpha: 1, delay: 0.5})
   //document.getElementById('message').innerHTML = outcome['message'];
+}
+
+function next_round() {
+  gsap.timeline()
+    .to("#result img", {duration: 0.5, autoAlpha: 0, stagger: 0.2})
+    .set("#choose", {display: "block"})
+    .to("#choose img", {duration: 0.5, autoAlpha: 1})
+    .to(".header", {duration: 1, autoAlpha: 1});
+  // document.getElementById('result').style.visibility = "hidden";
+  document.getElementById('result').style.display = "none";
+}
+
+function reset() {
+  score = 0;
+  rounds = 0;
+  success = 0;
+  document.getElementById('score').innerHTML="";
+  next_round();
+}
+
+function addQueryParam(a, b) {
+  return a + "/?" + b
 }
 
 function distort(r) {
@@ -74,6 +79,11 @@ function compResult(r) {
   return toggle[r]
 }
 
+function get_score(r) {
+  var result_to_score = { 'win': 1, 'lose': 0, 'draw': 0.5 }
+  return result_to_score[r]
+}
+
 function imagePick() {
   const choices = ['tiger', 'yoda', 'puppy'];
   return choices[Math.floor(Math.random() * 3)];
@@ -82,7 +92,7 @@ function imagePick() {
 function decideWinner(human, bot) {
   switch(human + ' ' + bot) {
   case 'tiger tiger':
-    return {'message': '2 tigers roar at eachother. Fair draw!', 'humanResult': 'draw'};
+    return {'message': '2 tigers roar at eachother. Draw!', 'humanResult': 'draw'};
     break;
   case 'tiger puppy':
     return {'message': 'Tiger devours puppy. You win!', 'humanResult': 'win'};
@@ -94,16 +104,16 @@ function decideWinner(human, bot) {
     return {'message': 'Puppy devoured by tiger. You lose!', 'humanResult': 'lose'};
     break;
   case 'puppy puppy':
-    return {'message': 'Puppies cannot outcute eachother. Fair draw!', 'humanResult': 'draw'};
+    return {'message': 'Puppies cannot outcute eachother. Draw!', 'humanResult': 'draw'};
     break;
   case 'puppy yoda':
-    return {'message': 'Puppy cuteness overhwelm Yoda is there. You win!', 'humanResult': 'win'};
+    return {'message': 'Puppy cuteness overhwelm Yoda is there. Win!', 'humanResult': 'win'};
     break;
   case 'yoda tiger':
-    return {'message': 'Fear is not there. Yoda scratches tiger belly. You win!', 'humanResult': 'win'};
+    return {'message': 'Fear is not there. Yoda scratches tiger belly. Win!', 'humanResult': 'win'};
     break;
   case 'yoda puppy':
-    return {'message': 'Yoda crushed by puppy cuteness. You lose!', 'humanResult': 'lose'};
+    return {'message': 'Yoda crushed by puppy cuteness. Lose!', 'humanResult': 'lose'};
     break;
   case 'yoda yoda':
     return {'message': 'Great harmony comes. Draw happens!', 'humanResult': 'draw'};
